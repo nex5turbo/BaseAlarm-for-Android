12999594a1f45398010d1f098ecfab8e652f5bf1
wonyoung <nex5turbo@gmail.com>
2021-07-29 12:49:41 +0900

    First Commit

--- app/src/main/java/won/young/basealarm/AlarmAlertActivity.kt
+++ app/src/main/java/won/young/basealarm/AlarmAlertActivity.kt
@@ -3,9 +3,15 @@ package won.young.basealarm
 import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
 import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
@@ -13,6 +19,13 @@ import won.young.basealarm.databinding.ActivityAlarmAlertBinding
 
 class AlarmAlertActivity : AppCompatActivity() {
     lateinit var binding: ActivityAlarmAlertBinding
+
+    private var isPlaying = false
+    private var mediaPlayer: MediaPlayer? = null
+
+    private var vibrator: Vibrator? = null
+    private val pattern = longArrayOf(100,200,300,400,300,200)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
@@ -28,12 +41,56 @@ class AlarmAlertActivity : AppCompatActivity() {
                     or WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
         }
         binding = DataBindingUtil.setContentView(this, R.layout.activity_alarm_alert)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        mediaPlayer = MediaPlayer()
+        initListener()
+        play()
+    }
+
+    private fun initListener() {
         binding.dismissButton.setOnClickListener {
-            Log.d("###", "clicked")
-            val stopAlarm = Intent(this, AlarmService::class.java)
-            stopAlarm.setAction("alarm_alert")
-            stopService(stopAlarm)
+            stop()
+            WakeLock.releaseCpuLock()
             finish()
+        }
+    }
+    override fun onBackPressed() {
+        if (isPlaying) return
+        super.onBackPressed()
+    }
+
+    private fun play() {
+        isPlaying = true
+        val mediaURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+
+        mediaPlayer!!.setDataSource(this, mediaURI)
+        startAlarm(mediaPlayer!!)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val amplitude = intArrayOf(20,20,20,20,20,20)
+            val waveform = VibrationEffect.createWaveform(pattern, amplitude, 0)
+            vibrator!!.vibrate(waveform)
+        } else {
+            vibrator!!.vibrate(pattern, 0)
+        }
+    }
+
+    private fun startAlarm(player: MediaPlayer) {
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, (audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM) * 0.7).toInt(), AudioManager.FLAG_PLAY_SOUND)
+        player.setAudioStreamType(AudioManager.STREAM_ALARM)
+        player.isLooping = true
+        player.prepare()
+        player.start()
+    }
+
+    private fun stop() {
+        if (isPlaying) {
+            isPlaying = false
+            mediaPlayer!!.stop()
+            mediaPlayer!!.release()
+            mediaPlayer = null
+            vibrator!!.cancel()
         }
     }
 }
--- app/src/main/java/won/young/basealarm/AlarmReceiver.kt
+++ app/src/main/java/won/young/basealarm/AlarmReceiver.kt
@@ -3,16 +3,16 @@ package won.young.basealarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+
 
 const val TAG = "AlarmReceiver"
 class AlarmReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d(TAG, "received")
+        WakeLock.acquireCpuWakeLock(context!!.applicationContext)
 
-        val playAlarm = Intent(context, AlarmService::class.java)
-        playAlarm.setAction("alarm_alert")
-        context!!.startService(playAlarm)
+        Log.d(TAG, "received")
 
         val intent = Intent(context, AlarmAlertActivity::class.java)
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_USER_ACTION)
